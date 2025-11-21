aca0e5ad4d382aa09e50d2bd8ab591d732b328e6
sanjaych90 <sanjayiam@outlook.com>
2025-11-21 15:42:22 +0530

    Initial commit

---
+++ Task 1 - Mathematical operations.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second (non-zero)number: "))
+
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
