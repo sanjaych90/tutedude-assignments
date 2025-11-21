78a23318205dc2cd31b35b97c5dc1c8665756860
sanjaych90 <sanjayiam@outlook.com>
2025-11-21 16:37:50 +0530

    Initial commit

---
+++ Task 2 - String concat.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+fullName = firstName + " " + lastName
+print(f"Hello, {fullName}! Welcome to Python program!")
+# print("Hello,", firstName, lastName+"!", "Welcome to Python program!")
